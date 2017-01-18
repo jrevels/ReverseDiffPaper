6c298ca5ee2d54fadf0b427885fbd7c34016c008
ccxxxx <chenxin715@gmail.com>
2017-01-18 10:58:28 -0500

    Clean up codes, add notes

--- BenchmarkPython/mnist.jl
+++ BenchmarkPython/mnist.jl
@@ -103,39 +103,39 @@ ReverseDiff.@forward minus_log(x::Real) = -log(x)
 
 function dim_logsumexp(x, dim)
     # take the log(sum(exp)) of a 2-d array in a numerical stable way
-    # exclude extreme cases
+    # excluding extreme cases
     # Note: we need something to check for NaNs and throw error message
     S = typeof(x)
     isempty(x) && return -S(Inf)
     u = max(0, maximum(x))
     abs(u) == Inf && return any(isnan, x) ? S(NaN) : u
-    # the actual calculation
+    # the actual formula
     return log(sum(exp(x - u), dim)) + u
 end
 
-function softmax_cross_entropy(y′, x)
+function softmax_cross_entropy(y′, y)
     # compte softmax and cross entropy together in a numerical stable way
     # since the outputing softmax is a ratio, rebase_x produces the same result as original x.
-    max_x = max(0, maximum(x))
-    rebase_x = x - max_x
-    logsumexp = log(sum(exp(rebase_x), 1))
-    y = exp(rebase_x - repmat(logsumexp, size(rebase_x, 1), 1))
+    max_y = max(0, maximum(y))
+    rebase_y = y - max_y
+    logsumexp = log(sum(exp(rebase_y), 1))
+    softmax = exp(rebase_y - repmat(logsumexp, size(rebase_y, 1), 1))
 
     # # The following way is safer: it deals with the extreme cases
     # # with dim_logsumexp(), but don't know how to check NaNs with tape.
     # y = exp(rebase_x - repmat(dim_logsumexp(rebase_x, 1), size(rebase_x, 1), 1))
 
     # constrain on the extreme values of y
-    y = min(y, 1 - eps(eltype(y)))
-    y = max(y, eps(eltype(y)))
+    softmax = min(softmax, 1 - eps(eltype(softmax)))
+    softmax = max(softmax, eps(eltype(softmax)))
 
     # A more general definition of cross entropy
     # entropy = mean(sum(y′ .* (minus_log.(y)), 1))
-    # The special case for binary y′
-    entropy = mean(sum(y′ .* minus_log.(y) + (1 - y′) .* minus_log.(1 - y), 1) ./ CLASS_COUNT)
-    # test without log(): no NaNs at all. There might be values <= 0
-    # in y that result in NaNs for large learning rates.
-    # entropy = mean(-sum(y′ .* y, 1))
+    # The special case for binary y
+    # Explanation here: https://en.wikipedia.org/wiki/Cross_entropy#Cross-entropy_error_function_and_logistic_regression
+    entropy = mean(sum(y′ .* minus_log.(softmax) + (1 - y′) .* minus_log.(1 - softmax), 1) ./ CLASS_COUNT)
+    # Note: it is confirmed that the NaNs were because of the log() function.
+    # There must be some y <= 0 that result in NaNs for large learning rates.
     return entropy
 end
 
@@ -197,16 +197,19 @@ function train_batch!(∇batch::Batch, batch::Batch, rate, labels)
     end
 end
 
-# rate = 2.81048e-4
+# Note: for BATCH_SIZE = 100, rate ≈ 3.227e-4 is the edge of producing
+# NaNs, i.e. rate = 3.227e-4 will produce NaNs within 5 iterations
+# It is now confirmed that the NaNs are produced because of log() in
+# the cross_entropy, so we need to throw an error/warning message when
+# NaNs happen.
 function train_all!(∇batch::Batch, batch::Batch, images, labels, rate = 4.2e-5, iters = 1000)
     # batch_count = floor(Int, size(images, 2) / BATCH_SIZE)
     for i in 1:iters
         # The usage of batch size is to train model on smaller sets
         # so as to use GPU (or distributed computation) later
         batch_ofs = (i - 1) * BATCH_SIZE + 1
-        while batch_ofs > 60000
-            batch_ofs -= 60000
-        end
+        batch_ofs = batch_ofs > size(TRAIN_IMAGES, 2) ? batch_ofs % size(TRAIN_IMAGES, 2) : batch_ofs
+
         load_batch!(batch, images, labels, batch_ofs)
         train_batch!(∇batch, batch, rate, labels)
         # Warning: the following line is never excuted
@@ -263,11 +266,15 @@ end
 
 # This function is used to find the best learning rate by running
 # the model with given num_iters and print test accuracies
-function choose_params(start_rate = 3e-5, step_length = .5e-5, num_step = 10, num_iters = 1000)
+# Input: start_rate: the learning rate to start with
+#        step_size: step size for learning rate
+#        num_step: number of different learning rates you want to try
+#        num_iters: the param to pass into train_all!()
+function choose_params(start_rate = 3e-5, step_size = .5e-5, num_step = 10, num_iters = 1000)
     result = zeros(num_step)
     rate = start_rate
     for i in 1:num_step
-        rate += step_length
+        rate += step_size
         batch = Batch(TRAIN_IMAGES, TRAIN_LABELS, 1);
         ∇batch = Batch(TRAIN_IMAGES, TRAIN_LABELS, 1);
 
@@ -275,5 +282,5 @@ function choose_params(start_rate = 3e-5, step_length = .5e-5, num_step = 10, nu
         result[i] = test_model(batch)
         println((i, rate, result[i]))
     end
-    return rate, result
+    return result
 end
